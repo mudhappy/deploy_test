23311813cb595510193b2a131cefc4349ad16abe
mudhappy <mudhappy@hotmail.com>
2016-11-23 13:34:30 -0500

    Add capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+set :stage, :production
+server '198.199.86.46', user: 'deployer', roles: %w{web app db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
